2d873d7a8acf85625b2515c5d37a0b7f00923dce
pinkeyrye <3tanalien@gmail.com>
2024-10-03 18:28:30 +0800

    Update: write urls to .txt file

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import urllib.request
-from urllib.parse import urljoin, urlparse
-import os
 import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 def main():
-    url = "https://www.pinterest.com/search/pins/?q=vtuber%20model"
+    query = "vtuber model"
+    url = "https://www.pinterest.com/search/pins/?q=" + query
+    img_set = set()
     driver = webdriver.Chrome()
     driver.get(url)
-    time.sleep(10) # 如果没有sleep页面会立刻关闭，元素来不及读取
-    images = driver.find_elements(By.XPATH, '//div[@data-test-id="pin-visual-wrapper"]//img')
-    # div[@data-test-id="pin-visual-wrapper"]//img/@src
-    # //div[@data-test-id="pin-visual-wrapper"]
-    for img in images:
-        src = img.get_attribute('src')
-        src = src.replace('236x', 'originals')
-        print(src)
+    time.sleep(10)  # 如果没有sleep页面会立刻关闭，元素来不及读取
+    limit = 5  # 滑动5次可以下载18张图片
+    for i in range(limit):
+        images = driver.find_elements(By.XPATH, '//div[@data-test-id="pin-visual-wrapper"]//img')
+        # div[@data-test-id="pin-visual-wrapper"]//img/@src
+        # //div[@data-test-id="pin-visual-wrapper"]
+        for img in images:
+            src = img.get_attribute('src')
+            src = src.replace('236x', 'originals')
+            img_set.add(src)
+        driver.execute_script('window.scrollBy(0, window.innerHeight);')
+    filename = f"{query}.txt"
+    with open(filename, "w") as file:
+        for elem in img_set:
+            print(elem)
+            file.write(elem + "\n")
+
+    print("size:", len(img_set))
     # print(images)
-    # TODO: jiliguala
-    # Press the green button in the gutter to run the script.
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
