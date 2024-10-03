8d16a698baee6bc4747e428369d8cbce793f4927
pinkeyrye <3tanalien@gmail.com>
2024-10-03 17:09:08 +0800

    Lazy first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import urllib.request
+from urllib.parse import urljoin, urlparse
+import os
+import time
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def main():
+    url = "https://www.pinterest.com/search/pins/?q=vtuber%20model"
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(10) # 如果没有sleep页面会立刻关闭，元素来不及读取
+    images = driver.find_elements(By.XPATH, '//div[@data-test-id="pin-visual-wrapper"]//img')
+    # div[@data-test-id="pin-visual-wrapper"]//img/@src
+    # //div[@data-test-id="pin-visual-wrapper"]
+    for img in images:
+        print(img.get_attribute('src'))
+    # print(images)
+    # TODO: jiliguala
+    # Press the green button in the gutter to run the script.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
